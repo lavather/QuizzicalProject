0023bdc04a30b8ac20c1f156076c7c5b921598ad
lavather <114093231+lavather@users.noreply.github.com>
2025-08-04 16:31:43 +0100

    Fix button styling by ensuring proper formatting and adding a newline at the end of the file

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,21 @@ import { QuizContext } from "./QuizContext.jsx"
 export default function App(){
     const [QuestionsArray,setQuestionsArray]=React.useState([])
     const [quizStarted, setQuizStarted] = React.useState(false)
+
+    function decodeHTML(str){
+        try { return decodeURIComponent(atob(str).split('').map(function(c) {return '%' + ('00' +c.charCodeAt(0).toString(16)).slice(-2);
+                    }).join('')); }
+        catch (e) { 
+            return atob(str); }
+    }
     
     React.useEffect(()=> {
         fetch('https://opentdb.com/api.php?amount=5&encode=base64')         
         .then(response => response.json())
          .then(data => {const QuestionsObjectArray= data.results.map(questionObjectEncoded=>{
-                                                        const question= atob(questionObjectEncoded.question)
-                                                        const correctAnswer=atob(questionObjectEncoded.correct_answer) 
-                                                        const wrongAnswers=questionObjectEncoded.incorrect_answers.map(answer=>atob(answer))
+                                                        const question= decodeHTML(questionObjectEncoded.question)
+                                                        const correctAnswer=decodeHTML(questionObjectEncoded.correct_answer) 
+                                                        const wrongAnswers=questionObjectEncoded.incorrect_answers.map(answer=>decodeHTML(answer))
                                                         const allAnswersArray=[correctAnswer,...wrongAnswers]
                                                         const shuffledAnswersArray=allAnswersArray.sort(()=>Math.random() - 0.5)
                                                         return{ question:question,
